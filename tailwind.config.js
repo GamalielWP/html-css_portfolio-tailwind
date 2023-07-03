5b4d1af999b006600b7ed898432972fa7cdad00b
Gamaliel Widhi Pradana <board.dabil@gmail.com>
2023-07-03 21:55:33 +0700

    About Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,8 @@ module.exports = {
     extend: {
       colors: {
         primary: '#14b8a6',
-        dark: '#0f172a'
+        dark: '#0f172a',
+        secondary: '#64748b'
       },
       screens: {
         '2xl': '1320px'
