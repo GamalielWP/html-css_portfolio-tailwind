00d75a56877f91756e7c8e9281122e01b3fced77
Gamaliel Widhi Pradana <board.dabil@gmail.com>
2023-07-05 14:48:03 +0700

    Refactor & Dark Mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,11 +3,14 @@ window.onscroll = function () {
     const header = document.querySelector('header');
     // menghitung jarak header dengan batas atas window
     const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
 
     if (window.scrollY > fixedNav) {
         header.classList.add('navbar-fixed');
+        toTop.classList.replace('hidden', 'flex');
     } else {
         header.classList.remove('navbar-fixed');
+        toTop.classList.replace('flex', 'hidden');
     }
 };
 
@@ -19,3 +22,33 @@ hamburger.addEventListener('click', function () {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
+
+// Klik di luar hamburger
+window.addEventListener('click', function (e) {
+    if (e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
+});
+
+// Darkmode toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function () {
+    if (darkToggle.checked) {
+        html.classList.replace('light', 'dark');
+        localStorage.theme = 'dark';
+    }
+    else {  
+        html.classList.replace('dark', 'light');
+        localStorage.theme = 'light';
+    }
+});
+
+// Pindahkan posisi toggle sesuai mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+  } else {
+    darkToggle.checked = false;
+  }
